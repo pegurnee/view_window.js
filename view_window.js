3462b516a22f3e8fea716a1b84b8d799895d7b2d
Eddie Gurnee <pegurnee@gmail.com>
2015-07-13 10:51:55 -0400

    specific view showing up of course

--- view_window.js
+++ view_window.js
@@ -1,4 +1,5 @@
 function setViewWindow(viewSelector) {
+  "use strict";
   var holder = document.getElementById(viewSelector);
   var views = document.getElementsByClassName(viewSelector);
   var selector = document.createElement('div');
@@ -7,12 +8,21 @@ function setViewWindow(viewSelector) {
   holder.insertBefore(selector, holder.firstChild);
 
   for (var i = 0; i < views.length; i++) {
-    var view = views[i];
+    let view = views[i];
     var newSelector = document.createElement('div');
     newSelector.id = view.id + '-selector';
     newSelector.innerHTML = view.id.charAt(0).toUpperCase() + view.id.substring(1);
     newSelector.style.display = 'inline-block';
-
+    newSelector.onclick = function () {
+      showView(views, view);
+    }
     selector.appendChild(newSelector);
   }
+}
+function showView(views, targetView) {
+  for (var i = 0; i < views.length; i++) {
+    var view = views[i];
+    view.style.display = 'none';
+  }
+  targetView.style.display = 'block';
 }
